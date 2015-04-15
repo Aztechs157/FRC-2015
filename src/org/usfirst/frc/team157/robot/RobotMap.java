614f2110e0e01278e1eb2979656f8bcd1e5b83f5
Tejasvi Nareddy <tejunareddy@gmail.com>
2015-04-14 20:13:17 -0400

    Started commenting RobotMap

--- src/org/usfirst/frc/team157/robot/RobotMap.java
+++ src/org/usfirst/frc/team157/robot/RobotMap.java
@@ -7,22 +7,27 @@ import edu.wpi.first.wpilibj.Gyro;
 import edu.wpi.first.wpilibj.can.CANMessageNotFoundException;
 
 /**
- *
+ * Defines all of the hardware port numbers for each hardware component, and then constructs each component for use by other
+ * subsystems. This makes it easy for one to change the IDs
  *
  * @author Teju Nareddy
  * 
  */
 public class RobotMap
 {
-	// Hardware IDs
+	// Hardware IDs are declared before
+	
+	// Drive Jaguar IDs
 	private static final int DRIVE_LEFT_JAG1_ID = 2;
 	private static final int DRIVE_LEFT_JAG2_ID = 4;
 	private static final int DRIVE_RIGHT_JAG1_ID = 7;
 	private static final int DRIVE_RIGHT_JAG2_ID = 10;
 	
+	// Subsystem Jaguar IDs
 	private static final int FORKS_JAG_ID = 11;
 	private static final int ELEVATOR_JAG_ID = 13;
 	
+	// Digital IDs
 	private static final int DRIVE_QUAD_ENCODER_LEFT_IDA = 1;
 	private static final int DRIVE_QUAD_ENCODER_LEFT_IDB = 2;
 	private static final int DRIVE_QUAD_ENCODER_RIGHT_IDA = 3;
@@ -32,10 +37,15 @@ public class RobotMap
 	private static final int ELEVATOR_HIGH_LIMSWITCH_ID = 9;
 	private static final int ELEVATOR_LOW_LIMSWITCH_ID = 8;
 	
+	// Analog IDs
 	private static final int ANALOG_SWITCH_ID = 0;
 	private static final int ANALOG_GYRO_ID = 1;
 	
-	// Jag Scaling Values
+	// -----------------------------------------//
+	// -----------------------------------------//
+	
+	// Jag Scaling Values. If the Jags are too fast or slow, changing this value will change the Jag's speed in every single file.
+	// Value between 0 and 1. For example, the drive jags are reduced to 1/2 their power.
 	private static final double DRIVE_LEFT_JAG1_SCALE = 0.5;
 	private static final double DRIVE_LEFT_JAG2_SCALE = 0.5;
 	private static final double DRIVE_RIGHT_JAG1_SCALE = -0.5;
@@ -53,38 +63,42 @@ public class RobotMap
 	// -----------------------------------------//
 	// -----------------------------------------//
 	
-	// Drive Subsystem Drive Wheels
+	// Declare Drive Subsystem Drive Wheels
 	public static ScaledCANJaguar driveLeftJag1;
 	public static ScaledCANJaguar driveLeftJag2;
 	public static ScaledCANJaguar driveRightJag1;
 	public static ScaledCANJaguar driveRightJag2;
 	
-	// Forklift Subsystem Jaguars
+	// Declare Forklift Subsystem Jaguars
 	public static ScaledCANJaguar forksJag;
 	public static ScaledCANJaguar elevatorJag;
 	
-	// Forklift Subsystem Limit Switches
+	// Declare Forklift Subsystem Limit Switches
 	public static DigitalLimitSwitch forksHighLimitSwitch;
 	public static DigitalLimitSwitch elevatorHighLimitSwitch;
 	public static DigitalLimitSwitch forksLowLimitSwitch;
 	public static DigitalLimitSwitch elevatorLowLimitSwitch;
 	
-	// Built-In Accelerometer: Most likely part of the Drive Subsystem
+	// Declare Built-In Accelerometer
 	public static BuiltInAccelerometer accelerometer;
 	
-	// Gyro
+	// Declare Gyro Sensor
 	public static Gyro gyro;
 	
-	// Quad Encoders
+	// Declare Quad Encoders
 	public static Encoder driveQuadEncoderLeft;
 	public static Encoder driveQuadEncoderRight;
 	
-	// Auto switch
+	// Declare Auto switch
 	public static AnalogSwitch autoSwitch = new AnalogSwitch(ANALOG_SWITCH_ID);
 	
+	/**
+	 * This method is called to instantiate each hardware component
+	 */
 	public static void init()
 	{
-		// Instantiate all of the jaguars with the scaling factors
+		// Instantiate all of the jaguars with the scaling factors and IDs. Surround with try/catch in case the jaguars are not
+		// plugged in. This way, the whole robot will not crash, only the individual jaguar will be disabled.
 		try
 		{
 			driveLeftJag1 = new ScaledCANJaguar(DRIVE_LEFT_JAG1_ID, DRIVE_LEFT_JAG1_SCALE);
